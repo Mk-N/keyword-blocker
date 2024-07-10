768f4b26aa17f38df8164a8b316b51aea053e06b
Mk-N <Reachtomukunth@gmail.com>
2024-07-10 10:44:55 +0100

    Add basic logging, fix paths & and space

--- site-blocker/src/offscreen.js
+++ site-blocker/src/offscreen.js
@@ -1,24 +1,28 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log("Offscreen document received message:", request.action);
   if (request.action === "checkUrl") {
-    const worker = new Worker(chrome.runtime.getURL("matcher.js"));
+    const worker = new Worker(chrome.runtime.getURL("src/matcher.js"));
     worker.postMessage({
       url: request.url,
       blockedKeywords: request.blockedKeywords,
     });
     worker.onmessage = (e) => {
+      console.log("Worker completed URL check.");
       sendResponse({ keyword: e.data });
       worker.terminate();
     };
     return true; // Indicates that the response is sent asynchronously.
   } else if (request.action === "optimizeKeywords") {
-    const worker = new Worker(chrome.runtime.getURL("optimizer.js"));
+    const worker = new Worker(chrome.runtime.getURL("src/optimizer.js"));
     worker.postMessage(request.keywords);
     worker.onmessage = (e) => {
+      console.log("Worker completed keyword optimization.");
       sendResponse({ optimizedKeywords: e.data });
       worker.terminate();
     };
     return true; // Indicates that the response is sent asynchronously.
   } else if (request.action === "checkOffscreen") {
+    console.log("checkOffscreen received in offscreen.js");
     sendResponse({ exists: true });
   }
 });
